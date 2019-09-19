7b12dd925b01ffa55669592cdb770254dcfce289
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-19 13:23:46 +0300

    project 1 init

--- Project1SwiftUI/ContentView.swift
+++ Project1SwiftUI/ContentView.swift
@@ -6,11 +6,59 @@
 //  Copyright © 2019 com.ri. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
-struct ContentView: View {
+class DataSource: ObservableObject {
+    @State private var hidesNavigationBar = false
+    let didChange = PassthroughSubject<Void, Never>()
+    var pictures = [String]()
+    
+    init() {
+        let fm = FileManager.default
+        
+        if let path = Bundle.main.resourcePath, let items = try? fm.contentsOfDirectory(atPath: path) {
+            
+            for item in items {
+                if item.hasPrefix("nssl") {
+                    pictures.append(item)
+                }
+            }
+        }
+        
+        didChange.send(())
+    }
+}
+
+struct DetailView: View {
+    var selectedImage: String
+    
     var body: some View {
-        Text("Hello World")
+        let img = UIImage(named: selectedImage)!
+        return Image(uiImage: img)
+        .resizable()
+        .aspectRatio(1024/768, contentMode: .fit)
+        .navigationBarTitle(Text(selectedImage),
+                            displayMode: .inline)
+        .navigationBarHidden(false)
+            .onTapGesture {
+                // add toggle gesture here
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var dataSource = DataSource()
+    
+    var body: some View {
+        NavigationView {
+            List(dataSource.pictures, id: \.self) { picture in
+               
+                NavigationLink(destination: DetailView(selectedImage: picture)) {
+                    Text(picture)
+                }
+            }.navigationBarTitle(Text("Storm Viewer"))
+        }
     }
 }
 
